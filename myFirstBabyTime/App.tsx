b8e73503db268ea410deea6f7d4aeae56b3abb5f
parkjaehoon <yukihoon12@naver.com>
2021-03-30 11:22:35 +0900

    react-native setting

---
+++ myFirstBabyTime/App.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+interface Props {}
+
+const App: React.FC<Props> = () => {
+  return <Text>sadsadsad</Text>;
+};
+export default App;
